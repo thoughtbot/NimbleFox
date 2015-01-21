54c8b24c89755b0aaa0862eba6add022e9da2128
Gordon Fontenot <gordon@thoughtbot.com>
2015-01-21 17:07:08 -0500

    Add frameworks
    This adds the basic frameworks that will be built.

---
+++ Source/Resources/NimbleFox.h
@@ -0,0 +1,19 @@
+//
+//  NimbleFox.h
+//  NimbleFox
+//
+//  Created by Gordon Fontenot on 1/21/15.
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NimbleFox.
+FOUNDATION_EXPORT double NimbleFoxVersionNumber;
+
+//! Project version string for NimbleFox.
+FOUNDATION_EXPORT const unsigned char NimbleFoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NimbleFox/PublicHeader.h>
+
+
