7bdd2fca7121ef1794dda8bd1b1d32ec47c4c4e5
Gordon Fontenot <gordon@thoughtbot.com>
2015-01-22 09:55:05 -0500

    Add dependencies with Carthage
    The project itself needs Fox and Nimble as dependencies, and we need
    Quick as a testing dependency.

---
+++ Tests/Tests/FoxMatchersSpec.swift
@@ -0,0 +1,12 @@
+import Fox
+import Nimble
+import NimbleFox
+import Quick
+
+class FoxMatchersSpec: QuickSpec {
+    override func spec() {
+        it("passes") {
+            expect(true).to(beTrue())
+        }
+    }
+}
