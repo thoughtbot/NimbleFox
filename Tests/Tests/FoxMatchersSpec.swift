108464059f6377bedd6fd5fd63cfe172164fd6f7
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-07-14 11:34:19 -0400

    Fix tests for Swift 1.2
    Apparently I never updated these tests for the syntax changes in 1.2.
    Whoops.

--- Tests/Tests/FoxMatchersSpec.swift
+++ Tests/Tests/FoxMatchersSpec.swift
@@ -5,8 +5,8 @@ import Quick
 
 func forAllIntegers(assertion: (Int, Int) -> Bool) -> FOXGenerator {
     return forAll(tuple([integer(), integer()])) { tuple in
-        let a = tuple[0] as Int
-        let b = tuple[1] as Int
+        let a = tuple[0] as! Int
+        let b = tuple[1] as! Int
 
         return assertion(a, b)
     }
